253946127a244376049422cbc588fe8a3b80032b
Emmanuel Bastien <os@ebastien.name>
2013-02-05 15:52:16 +0100

    Non-working example.

--- demo/gen.rb
+++ demo/gen.rb
@@ -6,22 +6,71 @@ module OISC
 
   class Instruction
     def bytecode(g)
-      g.meta_push_1
-      g.meta_push_1
-      g.meta_send_op_plus 0
-      g.ret
+      done = g.new_label
+      elze = g.new_label
+
+      g.push_local 0            # 1
+
+      g.push_local 0            # 2
+      g.push_local 1            # 3
+      g.meta_push_0             # 4
+      g.send :fetch, 2, false   # 2
+
+      g.push_local 0            # 3
+      g.push_local 1            # 4
+      g.meta_push_1             # 5
+      g.meta_send_op_plus 0     # 4
+      g.set_local 1
+      g.meta_push_0             # 5
+      g.send :fetch, 2, false   # 3
+
+      g.meta_send_op_minus 0    # 2
+
+      g.push_local 1            # 3
+      g.swap_stack
+      g.send :[]=, 2, false     # 1
+
+      g.meta_push_0             # 2
+      g.meta_send_op_lt 0       # 1
+      g.gif elze
+
+      g.push_local 1            # 2
+      g.meta_push_2             # 3
+      g.meta_send_op_plus 0     # 2
+
+      g.goto done
+      elze.set!
+
+      g.push_local 0            # 2
+      g.push_local 1            # 3
+      g.meta_push_1             # 4
+      g.meta_send_op_plus 0     # 3
+      g.meta_push_0             # 4
+      g.send :fetch, 2, false   # 2
+
+      done.set!
+
+      g.set_local 1
+      g.pop                     # 1
+    end
+  end
+
+  class Expression < Struct.new(:expr)
+    def bytecode(g)
+      expr.each { |e| e.bytecode(g) }
     end
   end
 
   class Lexer < Parslet::Parser
     alias_method :tokenize, :parse
     rule(:instr) { str 'I' }
-    rule(:expr) { instr.as :instr }
+    rule(:expr) { instr.as(:instr).repeat.as(:expr) }
     root :expr
   end
 
   class Parser < Parslet::Transform
     rule(:instr => simple(:instr)) { Instruction.new }
+    rule(:expr => subtree(:expr)) { Expression.new(expr) }
   end
 
   class Generator < Rubinius::Compiler::Stage
@@ -32,10 +81,34 @@ module OISC
       compiler.generator = self
     end
 
+    def memory
+      memory = [
+        1, 0, 0,
+        0, 1, 0,
+        0, 0, 0
+      ]
+      memory.each do |m|
+        @output.push_int m
+      end
+      @output.make_array memory.length
+      @output.set_local 0
+      @output.pop
+    end
+
     def run
       @output = Rubinius::Generator.new
       @output.set_line Integer(1)
+      
+      @output.meta_push_0
+      @output.set_local 1 # PC
+      
+      memory
+      #@output.cast_array
+      #@output.set_local 0 # Memory
+
       @input.bytecode @output
+      @output.use_detected
+      @output.push_local 0
       @output.ret
       @output.close
       run_next
@@ -81,4 +154,4 @@ module OISC
   end
 end
 
-puts OISC::compile("I").inspect
+puts OISC::compile("III").inspect
